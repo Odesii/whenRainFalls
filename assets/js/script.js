7ba4304d6aa1af75f08a3ed582b08f769298e332
Odesii <alic.madd@gmail.com>
2024-04-06 02:41:00 -0400

    started work on 5 day forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,7 +8,7 @@ button.addEventListener("click", function (event) {
   if (city) {
     getWeather(city);
     addHistory(city);
-    forecast(city);
+
   }
 });
 
@@ -29,47 +29,22 @@ function getWeather(city) {
       const longitude = data[0].lon;
       // console.log(latitude, longitude);
 
-      const forecast = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=imperial&appid=${weatherAppAPIKey}`;
+      const currentW = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&units=imperial&appid=${weatherAppAPIKey}`;
 
-      fetch(forecast)
+      fetch(currentW)
         .then(function (response2) {
           return response2.json();
         })
         .then(function (data2) {
         //   console.log("--------- Second request with current weather --------");
-        //   console.log(data2);
+          console.log(data2);
           displayCurrent(data2);
+          //five(data2);
         });
     });
 }
 
-function forecast(city){
-    const weatherUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${weatherAppAPIKey}`;
 
-    fetch(weatherUrl)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        // console.log("--------- First request with geolocation --------")
-        // console.log(data);
-  
-        const latitude = data[0].lat;
-        const longitude = data[0].lon;
-        // console.log(latitude, longitude);
-  
-        const forecast = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&units=imperial&appid=${weatherAppAPIKey}`;
-        fetch(forecast)
-        .then(function (response2) {
-          return response2.json();
-        })
-        .then(function (data2) {
-          console.log("--------- Second request with current weather --------");
-          console.log(data2);
-        });
-
-});
-}
 
 function displayCurrent(weatherData) {
     let currentWeatherEl = document.getElementById("mainWeather");
@@ -79,35 +54,51 @@ function displayCurrent(weatherData) {
   
     // Create and append the city name
     let cityName = document.createElement("h2");
-    cityName.textContent = weatherData.name;
+    cityName.textContent = weatherData.city.name;
     mainInsert.appendChild(cityName);
   
     // Create and append the weather icon
     let weatherIcon = document.createElement("img");
-    let baseUrl = `https://openweathermap.org/img/wn/${weatherData.weather[0].icon}.png`;
+    let baseUrl = `https://openweathermap.org/img/wn/${weatherData.list[0].weather[0].icon}.png`;
     weatherIcon.src = baseUrl;
     mainInsert.appendChild(weatherIcon);
   
     // Format and append the date
     
-    const date = dayjs.unix(weatherData.dt).format('MM-DD-YY');
+    const date = dayjs.unix(weatherData.list[0].dt).format('MM-DD-YY');
     let dateEl = document.createElement("h3");
     
     dateEl.textContent = date; 
     mainInsert.appendChild(dateEl);
   
-let windSpeed = weatherData.wind.speed
-    windSpeed = Math.floor(weatherData.wind.speed / 1.467)
+  let windSpeed = Math.floor(weatherData.list[0].wind.speed / 1.467)
 
     // Create and append the weather details
     let weatherDetails = document.createElement("p");
-    weatherDetails.textContent = `Temp ${weatherData.main.temp}°F, Humidity ${weatherData.main.humidity}%, Wind Speed ${windSpeed}MPH`;
+    weatherDetails.textContent = `Temp ${weatherData.list[0].main.temp}°F, Humidity ${weatherData.list[0].main.humidity}%, Wind Speed ${windSpeed}MPH`;
     mainInsert.appendChild(weatherDetails);
   
     // Append the main container to the current weather element
     currentWeatherEl.appendChild(mainInsert);
+
+
   }
   
+  function fiveDay(weatherData){
+    let five = document.getElementById("5day")
+    five.textContent = '';
+
+    let forecast = document.createElement("div");
+    let weather = weatherData.list; 
+    
+
+
+
+
+
+
+
+  }
 
 
 
